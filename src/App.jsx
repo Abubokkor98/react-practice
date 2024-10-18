35c4432d70863bff6d8fff37955447b0b61dc478
Abubokkor98 <mail.abubokkor@gmail.com>
2024-10-18 23:25:58 +0600

    data map

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,26 @@ import './todo.css'
 import Data from './data.json'
 
 function App() {
-  console.log(Data[0].desc);
+ console.log(Data);
+
+  let items = [];
+
+  // we will use this map loop
+  items = Data.map(item => <Card titleText={item.title} descText={item.desc}></Card>)
+
+  // we will never use for loop
+  // for (let x=0; x<Data.length; x++){
+  //   items.push()
+  // }
  
 
 
   return (
     <div>
       <h1 className='headingStyle'>Todo App</h1>
-      <Card titleText={Data[0].title} descText={Data[0].desc}></Card>
-      <Card titleText={Data[1].title} descText={Data[1].desc}></Card>
-      <Card titleText={Data[2].title} descText={Data[2].desc}></Card>
-      <Card titleText={Data[3].title} descText={Data[3].desc}></Card>
-      <Card titleText={Data[4].title} descText={Data[4].desc}></Card>
+      {items}
+      
+
     </div>
   )
 }
