dc106b27eabc365358de6eb80e85c2f0c6988561
Abubokkor98 <mail.abubokkor@gmail.com>
2024-10-18 22:38:44 +0600

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-
-// import './App.css'
-import Card from './Card';
+import Card from './components/Card';
 import './todo.css'
 
 function App() {
@@ -10,8 +8,10 @@ function App() {
   return (
     <div>
       <h1 className='headingStyle'>Todo App</h1>
-      <Card></Card>
-      <Card></Card>
+      <Card titleText="Call Mother" descText="Description one"></Card>
+      <Card titleText="Call Father" descText="Description two"></Card>
+      <Card titleText="Call Brother" descText="Description three"></Card>
+      <Card titleText="Call Sister" descText="Description four"></Card>
     </div>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-export default function Card() {
+export default function Card(props) {
+  console.log(props);
 
-    const todoTitle = 'Call Family';
-    const todoDesc = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, cumque.';
+    // const todoTitle = 'Call Family';
+    // const todoDesc = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, cumque.';
     const date = new Date();
     const fullDate = date.getDate();
     const month = date.getMonth();
@@ -12,8 +13,8 @@ export default function Card() {
   return (
     <div>
       <div className='card'>
-        <h3 className='cardTitle'>{todoTitle}</h3>
-        <p className='cardDesc'>{todoDesc}</p>
+        <h3 className='cardTitle'>Title:{props.titleText}</h3>
+        <p className='cardDesc'>Desc: {props.descText}</p>
         <p className='cardFooter'>{fullDate + "/" + month + "/" + currentYear}</p>
       </div>
     </div>
